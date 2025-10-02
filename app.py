91d9c426f62e0d032c2660df09e2be42e34f6104
FATHIMA SAFREENA <safreenafathima8@gmail.com>
2025-10-02 20:24:30 +0530

    app.py

--- app.py
+++ app.py
@@ -66,5 +66,6 @@ def clear_chat():
 # Run the app
 if _name_ == "_main_":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
+
 
